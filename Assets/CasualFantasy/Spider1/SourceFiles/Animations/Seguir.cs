b7a60c36357a7b9a96f77e9047f6405657585f8b
Jose Ortiz Ripoll <72159926+sose1102@users.noreply.github.com>
2021-10-25 00:48:15 +0200

    movimiento del compañero
    La araña sigue al personaje principal pero va del reves

--- Assets/CasualFantasy/Spider1/SourceFiles/Animations/Seguir.cs
+++ Assets/CasualFantasy/Spider1/SourceFiles/Animations/Seguir.cs
@@ -1,32 +1,23 @@
-/*
- *using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class seguir : MonoBehaviour
 {
-    public float speed;
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
 
-    Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
-        target = transform.position;
+        player = GameObject.FindGameObjectWithTag("DogPolyart").transform;
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            target = Camera.main.SceenToWorldPoint(Input.mousePosition);
-
-            target.z = 0f;
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deletaTime);
-
-        Debug.DrawLine(transform.position, target, Color.green);
+        nav.SetDestination(player.position);
     }
 }
- */ 
