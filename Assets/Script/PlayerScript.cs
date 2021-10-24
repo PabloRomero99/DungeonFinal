136310828972c276252ce3c6f14d6f83d7fea611
unknown <bujapetas@gamil.com>
2021-10-24 19:13:01 +0200

    Movimineto V1

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -6,11 +6,13 @@ public class PlayerScript : MonoBehaviour
 {
     public float turnSpeed = 20f;
 
-    public bool isRunning;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+    public bool estoyAtacando;
+    public float n_speed;
+    public bool IsRunning;
 
     void Start()
     {
@@ -18,15 +20,15 @@ public class PlayerScript : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
-        {
-            m_Animator.SetBool("IsRunning", false);
-            isRunning = false;
-        }
-        else { 
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            m_Animator.SetTrigger("Attack1");
+        }
+        if (!estoyAtacando)
+        {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
@@ -35,18 +37,15 @@ public class PlayerScript : MonoBehaviour
 
             bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
             bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-            bool isWalking = hasHorizontalInput || hasVerticalInput;
-            m_Animator.SetBool("IsRunning", isWalking);
-            isRunning = true;
+            IsRunning = hasHorizontalInput || hasVerticalInput;
+            m_Animator.SetBool("IsRunning", IsRunning);
 
             Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
             m_Rotation = Quaternion.LookRotation(desiredForward);
-        }
-    }
+            m_Rigidbody.MovePosition(transform.position + m_Movement * n_speed * Time.deltaTime);
+            m_Rigidbody.MoveRotation(m_Rotation);
 
-    void OnAnimatorMove()
-    {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation(m_Rotation);
+        }
+
     }
 }
